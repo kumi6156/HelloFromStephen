7bb55794cf5320e3a20eab0066da87c25fefc841
Nefi Aguilar <solemnefi153@gmail.com>
2021-05-22 13:42:17 -0600

    Nefi notes

--- src/Koomson/HelloFromStephen.java
+++ src/Koomson/HelloFromStephen.java
@@ -4,6 +4,7 @@ public class HelloFromStephen {
 
     public static void main(String[] args) {
         System.out.println("Hello my name is Stephen");
+        System.out.println("Hello Stephen, mt name is Nefi");
 	// write your code here
     }
 }
